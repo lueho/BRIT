6e95e2b639fb971c81fd7587f8c87212d66cdcac
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-02-24 18:14:37 +0100

    variations of filter views

--- trees/urls.py
+++ trees/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import ListView, TemplateView
 from djgeojson.views import GeoJSONLayerView
 from .models import HH_Roadside
 from rest_framework.routers import DefaultRouter
-from .views import TreeViewSet
+from .views import TreeViewSet, BootstrapTreeView
+from .views import treeSearch
 
 # router = DefaultRouter()
 # router.register(r'markers', TreeViewSet)
@@ -24,5 +25,7 @@ urlpatterns = [
     # path('', TreeList.as_view(template_name = 'tree_map.html')),
     path('', TemplateView.as_view(template_name = 'tree_map_json.html'), name='home_map'),
     # path('data.geojson', GeoJSONLayerView.as_view(model=HH_Roadside, properties=('gattung_deutsch', 'stammumfang', 'bezirk')), name='tree_data'),
-    path('data.geojson', tree_json, name='tree_data')
+    path('data.geojson', tree_json, name='tree_data'),
+    path('treesearch/', treeSearch, name='tree_search'),
+    path('bootstrap/', BootstrapTreeView, name='bootstrap'),
 ]
--- trees/views.py
+++ trees/views.py
@@ -3,9 +3,52 @@ from rest_framework_gis.filters import InBBoxFilter
 from .models import HH_Roadside
 from flexibi_dst.models import Districts_HH
 from .serializers import TreeSerializer
+from .filters import TreeFilter, TreeFilterSet
+from django.shortcuts import render
+from django_filters import rest_framework as filters
+from django.http import QueryDict
 
 class TreeViewSet(ReadOnlyModelViewSet):
-    bbox_filter_field = 'geom'
-    filter_backends = (InBBoxFilter, )
-    queryset = HH_Roadside.objects.filter(geom__isnull=False, gattung_deutsch='Erle', pflanzjahr__gt=1950, geom__intersects=Districts_HH.objects.get(name='Altona').geom)
+    queryset = HH_Roadside.objects.all()
     serializer_class = TreeSerializer
+    filter_backends = (filters.DjangoFilterBackend)
+    # filterset_fields = ('gattung_deutsch', 'bezirk', 'pflanzjahr', 'stammumfang',)
+    filterset_class = TreeFilterSet
+    # bbox_filter_field = 'geom'
+    # filter_backends = (InBBoxFilter, )
+    # queryset = HH_Roadside.objects.filter(geom__isnull=False, gattung_deutsch='Buche', pflanzjahr__gt=1980, geom__intersects=Districts_HH.objects.get(name='Bergedorf').geom)
+    
+def treeSearch(request):
+    queryset = HH_Roadside.objects.all()
+    tree_filter = TreeFilter(request.GET, queryset=queryset)
+    return render(request, 'tree_list.html', {'filter': tree_filter})
+    
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+
+def BootstrapTreeView(request):
+    qs = HH_Roadside.objects.all()
+    districts = Districts_HH.objects.all()
+    gattung_deutsch_query = request.GET.get('gattung_deutsch')
+    pflanzjahr_min_query = request.GET.get('pflanzjahr_min')
+    pflanzjahr_max_query = request.GET.get('pflanzjahr_max')
+    district_query = request.GET.get('district')
+
+    if is_valid_queryparam(gattung_deutsch_query):
+        qs = qs.filter(gattung_deutsch__icontains=gattung_deutsch_query)
+
+    if is_valid_queryparam(pflanzjahr_min_query):
+        qs = qs.filter(pflanzjahr__gte=pflanzjahr_min_query)
+
+    if is_valid_queryparam(pflanzjahr_max_query):
+        qs = qs.filter(pflanzjahr__lt=pflanzjahr_max_query)
+        
+    if is_valid_queryparam(district_query) and district_query != 'Bitte wählen...':
+        qs = qs.filter(bezirk__icontains=district_query)
+
+    context = {
+        'queryset': qs,
+        'districts': districts
+    }
+    return render(request, "bootstrap_form.html", context)
