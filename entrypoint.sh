0c40d18ac66323ee8d93ddd933611a27e369e77e
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-06-16 11:46:16 +0200

    Now using volume for persisting db data
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-python manage.py flush --no-input
+#python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 
