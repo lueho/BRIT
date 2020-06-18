27c173aac9bf464ef1d2410d891a8b9404550777
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-06-18 17:54:37 +0200

    Removed collectstatic for speed up during development
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 #python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --no-input --clear
+#python manage.py collectstatic --no-input --clear
 
 exec "$@"
