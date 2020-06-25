9c5a19bea5322d686f7bce21973ed5b8fd75734f
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-06-25 09:59:40 +0200

    Added user registration
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from .views import UserRegistrationView
+
 urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout')
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'users/register.html'
