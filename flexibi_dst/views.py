fae67712c6c7334bcc1d50becd384cbbebebf73e
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-06-29 17:56:10 +0200

    fixed DualUserListView errors
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/views.py
+++ flexibi_dst/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView, ListView
 
@@ -7,10 +8,12 @@ class HomeView(TemplateView):
 
 
 class DualUserListView(ListView):
-    standard_owner = User.objects.get(username='flexibi')
+    standard_owner = User.objects.get(username=settings.PUBLIC_OBJECT_OWNER)
+    model = None
+    object_list = None
 
     def get(self, request, *args, **kwargs):
-        standard_objects = self.model.objects.filter(owner=self.standard_owner)
+        standard_objects = self.model.objects.filter(owner=8)
         if request.user.is_authenticated:
             user_objects = self.model.objects.filter(owner=self.request.user)
         else:
