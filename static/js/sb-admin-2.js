561a05d8645cbd22a39a1c89e586d332f066fa24
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-07-08 11:35:45 +0200

    Unix compatibility
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- static/js/sb-admin-2.js
+++ static/js/sb-admin-2.js
@@ -7,14 +7,14 @@
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Close any open menu accordions when window is resized below 768px
   $(window).resize(function() {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
-    };
+    }
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
