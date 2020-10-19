f9cd044014c2d9bda14df7bf075c6fa3c61e4ab8
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-10-19 13:34:06 +0200

    fixed databas env variable
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings.py
+++ flexibi_dst/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
