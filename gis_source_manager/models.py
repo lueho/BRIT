d5e713275f63d71950b83901c31e247939b1b4a6
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-10-30 08:54:02 +0100

    introduced NantesGreenhouses model
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- gis_source_manager/models.py
+++ gis_source_manager/models.py
@@ -43,3 +43,28 @@ class HamburgGreenAreas(models.Model):
     # class Meta:
     # managed = False
     # db_table = 'trees_hh_parks'
+
+
+class NantesGreenhouses(models.Model):
+    geom = PointField(blank=True, null=True)
+    id_exp = models.CharField(max_length=255, blank=True, null=True)
+    nom_exp = models.CharField(max_length=255, blank=True, null=True)
+    id_serre = models.CharField(max_length=255, blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    surface_ha = models.CharField(max_length=255, blank=True, null=True)
+    nb_cycles = models.IntegerField(blank=True, null=True)
+    culture_1 = models.CharField(max_length=20, blank=True, null=True)
+    start_cycle_1 = models.CharField(max_length=20, blank=True, null=True)
+    end_cycle_1 = models.CharField(max_length=20, blank=True, null=True)
+    culture_2 = models.CharField(max_length=20, blank=True, null=True)
+    start_cycle_2 = models.CharField(max_length=20, blank=True, null=True)
+    end_cycle_2 = models.CharField(max_length=20, blank=True, null=True)
+    culture_3 = models.CharField(max_length=20, blank=True, null=True)
+    start_cycle_3 = models.CharField(max_length=20, blank=True, null=True)
+    end_cycle_3 = models.CharField(max_length=20, blank=True, null=True)
+    layer = models.CharField(max_length=20, blank=True, null=True)
+    heated = models.BooleanField(blank=True, null=True)
+    lighted = models.BooleanField(blank=True, null=True)
+    high_wire = models.BooleanField(blank=True, null=True)
+    above_ground = models.BooleanField(blank=True, null=True)
