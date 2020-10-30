3548689be997228687e3f1f871094ecd4a429265
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2020-10-30 17:24:26 +0100

    integrated NantesGreenhouse model to bioresource explorer
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- bioresource_explorer/forms.py
+++ bioresource_explorer/forms.py
@@ -1,8 +1,11 @@
-from django.forms import (IntegerField,
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.layout import Div, Field
+from django.forms import (ChoiceField, CheckboxSelectMultiple, MultipleChoiceField, RadioSelect, IntegerField,
+                          Form,
                           ModelForm,
                           TextInput)
 
-from gis_source_manager.models import HamburgRoadsideTrees
+from gis_source_manager.models import HamburgRoadsideTrees, NantesGreenhouses
 
 
 class HamburgRoadsideTreeFilterForm(ModelForm):
@@ -27,3 +30,72 @@ class HamburgRoadsideTreeFilterForm(ModelForm):
             'pflanzjahr_min': 'Planted earliest',
             'pflanzjahr_max': 'Planted latest',
         }
+
+
+CROP_CHOICES = (
+    (1, "Cucumber"),
+    (2, "Tomato"),
+)
+
+HEATING_CHOICES = (
+    (1, "All"),
+    (2, "Heated"),
+    (3, "Not heated")
+)
+
+LIGHTING_CHOICES = (
+    (1, "All"),
+    (2, "Lighting"),
+    (3, "No Lighting")
+)
+
+PROD_MODE_CHOICES = (
+    (1, "All"),
+    (2, "On Ground"),
+    (3, "Above Ground")
+)
+
+CULT_MANAGEMENT_CHOICES = (
+    (1, "All"),
+    (2, "Classic"),
+    (3, "High-Wire")
+)
+
+
+class Row(Div):
+    css_class = "form-row"
+
+
+class NantesGreenhousesFilterForm(Form):
+    crops = MultipleChoiceField(widget=CheckboxSelectMultiple, choices=CROP_CHOICES, initial=('1', '2'))
+    heating = ChoiceField(widget=RadioSelect, choices=HEATING_CHOICES, initial='1')
+    lighting = ChoiceField(widget=RadioSelect, choices=LIGHTING_CHOICES, initial='1')
+    prod_mode = ChoiceField(widget=RadioSelect, choices=PROD_MODE_CHOICES, initial='1')
+    cult_man = ChoiceField(widget=RadioSelect, choices=CULT_MANAGEMENT_CHOICES, initial='1')
+    helper = FormHelper()
+    helper.layout = Layout(
+        Row(
+            Field('heating', wrapper_class='col-md-6'),
+            Field('lighting', wrapper_class='col-md-6'),
+            Field('prod_mode', wrapper_class='col-md-6'),
+            Field('cult_man', wrapper_class='col-md-6'),
+            Field('crops', wrapper_class='col-md-12')
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['heating'].label = 'Heating'
+        self.fields['lighting'].label = 'Lighting'
+        self.fields['prod_mode'].label = 'Production Mode'
+        self.fields['cult_man'].label = 'Cultivation Management'
+        helper = FormHelper()
+        helper.layout = Layout(
+            Row(
+                Field('heating', wrapper_class='col-md-6'),
+                Field('lighting', wrapper_class='col-md-6')
+            )
+        )
+
+    class Meta:
+        model = NantesGreenhouses
--- bioresource_explorer/urls.py
+++ bioresource_explorer/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from .views import (BioresourceExplorerHomeView,
                     HamburgExplorerView,
                     HamburgRoadsideTreeAPIView,
-                    NantesExplorerView)
+                    NantesGreenhousesView,
+                    NantesGreenhousesAPIView,
+                    )
 
 urlpatterns = [
     path('', BioresourceExplorerHomeView.as_view(), name='bioresource_explorer_home'),
     path('data.hamburg_roadside_trees/', HamburgRoadsideTreeAPIView.as_view(), name='data.hamburg_roadside_trees'),
+    path('data.nantes_greenhouses/', NantesGreenhousesAPIView.as_view(), name='data.nantes_greenhouses'),
     path('hamburg/', HamburgExplorerView.as_view(), name='hamburg_explorer'),
-    path('nantes/', NantesExplorerView.as_view(), name='nantes_explorer'),
+    path('nantes/', NantesGreenhousesView.as_view(), name='nantes_greenhouses'),
 ]
