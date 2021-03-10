3733d023c65ca11f97282e1d76921bb20cc7ea78
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-10 17:55:22 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flexibi_dst.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flexibi_dst.settings.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
