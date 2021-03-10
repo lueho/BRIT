62bbbc19932bd35dc97a34d1f3a8db24c1aca691
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-10 22:03:32 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings/heroku.py
+++ flexibi_dst/settings/heroku.py
@@ -54,3 +54,7 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 WHITENOISE_MANIFEST_STRICT = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
