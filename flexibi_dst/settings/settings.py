619c0da08c970e224c491b78c888bbdcdd75c83b
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-10 17:29:15 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings/settings.py
+++ flexibi_dst/settings/settings.py
@@ -163,48 +163,48 @@ IMPORTED_CASE_STUDIES = [
     'flexibi_nantes',
 ]
 
-ADMINS = [('Phillipp Lüssenhop', 'phillipp.luessenhop@tuhh.de')]
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s [%(asctime)s] %(module)s %(message)s'
-        },
-    },
-    'handlers': {
-        # 'console': {
-        #     'level': 'DEBUG',
-        #     'class': 'logging.StreamHandler',
-        #     'formatter': 'simple'
-        # },
-        'file': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': os.path.join(BASE_DIR, 'logs', 'main.log'),
-            'maxBytes': 1024000,
-            'backupCount': 3,
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'mail_admins'],
-            'propagate': True,
-            'level': 'WARNING',
-        },
-    }
-}
+# ADMINS = [('Phillipp Lüssenhop', 'phillipp.luessenhop@tuhh.de')]
+# EMAIL_HOST = os.environ.get('EMAIL_HOST')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(levelname)s [%(asctime)s] %(module)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         # 'console': {
+#         #     'level': 'DEBUG',
+#         #     'class': 'logging.StreamHandler',
+#         #     'formatter': 'simple'
+#         # },
+#         'file': {
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'formatter': 'verbose',
+#             'filename': os.path.join(BASE_DIR, 'logs', 'main.log'),
+#             'maxBytes': 1024000,
+#             'backupCount': 3,
+#         },
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         }
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file', 'mail_admins'],
+#             'propagate': True,
+#             'level': 'WARNING',
+#         },
+#     }
+# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
