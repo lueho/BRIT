aa71a36be02c04be84d6bb57f778a67fba8e3772
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-10 18:30:08 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings/heroku.py
+++ flexibi_dst/settings/heroku.py
@@ -1,3 +1,6 @@
+import os
+
+import dj_database_url
 import environ
 
 env = environ.Env(
@@ -9,5 +12,9 @@ SECRET_KEY = env('SECRET_KEY')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 DATABASES = {
-    'default': env.db(),
+    'default': {}
 }
+DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
--- flexibi_dst/settings/settings.py
+++ flexibi_dst/settings/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-import dj_database_url
-
 # import environ
 #
 # env = environ.Env()
@@ -96,11 +94,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
