a8539de75d6d2b8bea021653c74c7cccdb4a7828
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-10 19:00:37 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings/heroku.py
+++ flexibi_dst/settings/heroku.py
@@ -1,7 +1,7 @@
-import os
-
 import dj_database_url
 import environ
+
+from .settings import *
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -11,9 +11,6 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-DATABASES = {
-    'default': {}
-}
 DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
