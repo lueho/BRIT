30d292fc46ec0f15990421d5dd1474ef190acf4b
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-11 19:38:35 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings/heroku.py
+++ flexibi_dst/settings/heroku.py
@@ -7,13 +7,15 @@ env = environ.Env(
 )
 
 DEBUG = env('DEBUG')
-SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
-DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+SECRET_KEY = env('SECRET_KEY', default=SECRET_KEY)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=ALLOWED_HOSTS)
+try:
+    DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
+    db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+except:
+    pass
 
 LOGGING = {
     'version': 1,
