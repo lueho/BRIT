6536760406a0432c35a73b5a3271d61543094abb
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-03-11 21:40:49 +0100

    settings fit for production environment
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/wsgi.py
+++ flexibi_dst/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flexibi_dst.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flexibi_dst.settings')
 
 application = get_wsgi_application()
