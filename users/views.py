37b1090ba38de8a8c499389448ec9780e295cc46
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-05-12 17:23:33 +0200

    Introduced ModalLoginRequiredMixin and ModalLoginRequiredMessage

--- users/views.py
+++ users/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, TemplateView
+from django.views.generic import TemplateView, CreateView, DeleteView, TemplateView
 from bootstrap_modal_forms.generic import BSModalLoginView
+from django.contrib.auth.mixins import AccessMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import CustomAuthenticationForm
 
 
@@ -35,5 +38,27 @@ class ModalLoginView(BSModalLoginView):
         context.update({
             'form_title': 'User Authentication',
             'submit_button_text': 'Login'
+        })
+        return context
+
+
+class ModalLoginRequiredMixin(AccessMixin):
+    """Verify that the current user is authenticated."""
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('loginrequiredmessage'))
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+
+class ModalLoginRequiredMessage(TemplateView):
+    template_name = 'modal_message.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'modal_title': 'Authentication required',
+            'modal_message': 'You are not allowed to access this. Please log in as the authorized user.'
         })
         return context
