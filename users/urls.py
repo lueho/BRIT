0cd9f437f27cb353560dfe121b0ed689c9f6ba12
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-05-12 16:46:01 +0200

    Login now with new ModalLoginView

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import UserProfileView, UserDeleteView, UserRegistrationView
+from .views import UserProfileView, UserDeleteView, UserRegistrationView, ModalLoginView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', ModalLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete')
 ]
