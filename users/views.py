cc69f900bf638615e75f09b9aca769da54c5b86f
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-05-12 16:50:33 +0200

    Introduced ModalLoginView

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, TemplateView
+from bootstrap_modal_forms.generic import BSModalLoginView
+from .forms import CustomAuthenticationForm
 
 
 class UserRegistrationView(CreateView):
@@ -20,3 +22,18 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
 class UserProfileView(LoginRequiredMixin, TemplateView):
     model = User
     template_name = 'user_profile.html'
+
+
+class ModalLoginView(BSModalLoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = 'login_modal.html'
+    success_message = 'Success: You were successfully logged in.'
+    extra_context = dict(success_url=reverse_lazy('login'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'form_title': 'User Authentication',
+            'submit_button_text': 'Login'
+        })
+        return context
