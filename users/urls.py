e637a74c2cdda036043e7b793e40daf45cf6f089
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-05-12 17:25:03 +0200

    Added path for login required message

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import UserProfileView, UserDeleteView, UserRegistrationView, ModalLoginView
+from .views import UserProfileView, UserDeleteView, UserRegistrationView, ModalLoginView, ModalLoginRequiredMessage
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('login/', ModalLoginView.as_view(), name='login'),
+    path('loginrequired/', ModalLoginRequiredMessage.as_view(), name='loginrequiredmessage'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete')
 ]
