f90ebd87ad565d413555c3b8717964fcda0c6c3a
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-06-09 22:07:42 +0200

    Extended LiteratureSource Model to general Source
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- library/models.py
+++ library/models.py
@@ -3,12 +3,31 @@ from django.db import models
 from django.urls import reverse
 
 
-class LiteratureSource(models.Model):
+class Licence(models.Model):
+    name = models.CharField(max_length=255)
+    reference_url = models.URLField()
+
+    def __str(self):
+        return self.name
+
+
+SOURCE_TYPES = (
+    ('website', 'website'),
+    ('custom', 'custom'),
+)
+
+
+class Source(models.Model):
     owner = models.ForeignKey(User, default=8, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255, choices=SOURCE_TYPES, default='custom')
     authors = models.CharField(max_length=500, null=True)
     title = models.CharField(max_length=500, null=True)
     abbreviation = models.CharField(max_length=50, null=True)
     abstract = models.TextField(blank=True, null=True)
+    licence = models.ForeignKey(Licence, on_delete=models.PROTECT, blank=True, null=True)
+    attributions = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    last_accessed = models.DateField(blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('literature source_detail', kwargs={'pk': self.id})
