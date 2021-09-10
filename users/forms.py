ba9a69ed3d48ab4ff6bf8717944fbf8de6553357
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-09-10 13:01:43 +0200

    Removed custom user registration form
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,14 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import EmailField
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = EmailField(max_length=200)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class CustomAuthenticationForm(AuthenticationForm):
