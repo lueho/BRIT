b0b38e8f7fab73fbb49185ba7535bef44409fd50
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-09-28 17:23:24 +0200

    Batch scripts now implemented as manage.py database commands

--- deploy_db.bat
+++
@@ -1,3 +0,0 @@
-cd db_dump
-for /f "usebackq tokens=*" %i in (`aws s3 presign s3://flexibi-dst/latest.dump`) do heroku pg:backups:restore -a flexibi-dst --confirm flexibi-dst "%i" HEROKU_POSTGRESQL_COPPER_URL
-cd..
