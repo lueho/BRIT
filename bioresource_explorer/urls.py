d42ca447ea890fdb5386c3fdf91a68b5c4ca5ee9
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-07 17:26:23 +0200

    added maps list

--- bioresource_explorer/urls.py
+++ bioresource_explorer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from .views import BioresourceExplorerHomeView
+from .views import BioresourceExplorerHomeView, MapsListView
 
 urlpatterns = [
     path('', BioresourceExplorerHomeView.as_view(), name='bioresource_explorer_home'),
+    path('maps_list/', MapsListView.as_view(), name='maps_list'),
     # TODO: Can case study urls be detected and added automatically?
     path('nantes/', include('case_studies.flexibi_nantes.urls')),
     path('hamburg/', include('case_studies.flexibi_hamburg.urls')),
