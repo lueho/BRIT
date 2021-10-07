1e1fce3334e54bb67c42caa16814be7d6f075f12
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-07 18:34:57 +0200

    Added media root for image storing

--- flexibi_dst/settings/local.py
+++ flexibi_dst/settings/local.py
@@ -14,5 +14,7 @@ DATABASES['default'] = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CRISPY_FAIL_SILENTLY = False
