d52980505f5e6db2fd29a0920a66e7a07902660b
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-07 19:56:19 +0200

    Added missing MEDIA_ROOT

--- flexibi_dst/settings/settings.py
+++ flexibi_dst/settings/settings.py
@@ -119,6 +119,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
