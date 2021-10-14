3e01ac0fb84343da57ffe1549094012019cf16cb
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-14 17:55:10 +0200

    Improved tree filter

--- case_studies/flexibi_hamburg/views.py
+++ case_studies/flexibi_hamburg/views.py
@@ -2,9 +2,9 @@ from django.http import JsonResponse
 from django.views.generic import TemplateView
 from rest_framework.views import APIView
 
-from .forms import HamburgRoadsideTreeFilterForm
 from .models import HamburgRoadsideTrees
 from .serializers import HamburgRoadsideTreeGeometrySerializer
+from .filters import TreeFilter
 
 
 class TreeFilterView(TemplateView):
@@ -14,7 +14,7 @@ class TreeFilterView(TemplateView):
         context = super().get_context_data(**kwargs)
         context.update({
             'map_header': 'Hamburg Roadside Trees',
-            # 'tree_filter': TreeFilter(self.request.GET),
+            'tree_filter': TreeFilter(self.request.GET),
         })
         return context
 
@@ -28,28 +28,31 @@ class HamburgRoadsideTreeAPIView(APIView):
     @staticmethod
     def get(request):
         qs = HamburgRoadsideTrees.objects.all()
-        gattung_deutsch_query = request.GET.get('gattung_deutsch')
-        pflanzjahr_min_query = request.GET.get('pflanzjahr_min')
-        pflanzjahr_max_query = request.GET.get('pflanzjahr_max')
-        district_query = request.GET.get('bezirk')
+        gattung_deutsch_query = request.query_params.getlist('gattung_deutsch[]')
+        # pflanzjahr_min_query = request.GET.get('pflanzjahr_min')
+        # pflanzjahr_max_query = request.GET.get('pflanzjahr_max')
+        district_query = request.query_params.getlist('bezirk[]')
 
         if is_valid_queryparam(gattung_deutsch_query):
-            qs = qs.filter(gattung_deutsch__icontains=gattung_deutsch_query)
+            qs = qs.filter(gattung_deutsch__in=gattung_deutsch_query)
 
-        if is_valid_queryparam(pflanzjahr_min_query):
-            qs = qs.filter(pflanzjahr__gte=pflanzjahr_min_query)
+        # if is_valid_queryparam(pflanzjahr_min_query):
+        #     qs = qs.filter(pflanzjahr__gte=pflanzjahr_min_query)
 
-        if is_valid_queryparam(pflanzjahr_max_query):
-            qs = qs.filter(pflanzjahr__lt=pflanzjahr_max_query)
+        # if is_valid_queryparam(pflanzjahr_max_query):
+        #     qs = qs.filter(pflanzjahr__lte=pflanzjahr_max_query)
 
-        if is_valid_queryparam(district_query) and district_query != 'Bitte wählen...':
-            qs = qs.filter(bezirk__icontains=district_query)
+        if is_valid_queryparam(district_query):
+            qs = qs.filter(bezirk__in=district_query)
 
         serializer = HamburgRoadsideTreeGeometrySerializer(qs, many=True)
         data = {
             'geoJson': serializer.data,
             'analysis': {
-                'tree_count': len(serializer.data['features'])
+                'tree_count': {
+                    'label': 'Number of trees',
+                    'value': len(serializer.data['features'])
+                },
             }
         }
 
