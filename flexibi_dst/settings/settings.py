5d96b262eb7d57ce924f53b679332ff3647d90a0
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-20 20:30:09 +0200

    activated debug toolbar
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/settings/settings.py
+++ flexibi_dst/settings/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
