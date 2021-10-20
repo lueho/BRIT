d28cb935b3e82171a1a63ae3a19fff7cb199a15f
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-20 20:24:10 +0200

    Added doughnut chart to material composition detail view
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- material_manager/views.py
+++ material_manager/views.py
@@ -263,42 +263,8 @@ class MaterialSettingsDetailView(UserPassesTestMixin, DetailView):
     def get_context_data(self, **kwargs):
         kwargs['composition'] = self.object.composition()
         charts = {}
-        for group_settings in kwargs['composition'].keys():
-            charts[f'composition-chart-{group_settings.id}'] = {
-                'type': 'doughnut',
-                'data': {
-                    'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    'datasets': [{
-                        'label': '# of Votes',
-                        'data': [12, 19, 3, 5, 2, 3],
-                        'backgroundColor': [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        'borderColor': [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        'borderWidth': 1,
-                        'hoverOffset': 4
-                    }]
-                },
-                'options': {
-                    'scales': {
-                        'y': {
-                            'beginAtZero': 'true'
-                        }
-                    }
-                }
-            }
+        for group_settings, content in kwargs['composition'].items():
+            charts[f'composition-chart-{group_settings.id}'] = content['averages_chart'].as_dict()
         kwargs['charts'] = charts
         kwargs['allow_edit'] = self.allow_edit
         return super().get_context_data(**kwargs)
