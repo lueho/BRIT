f59b66fa5c56ed13e61351a706aeaf53e5cfa842
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-24 15:39:09 +0200

    fixed favicon issue
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- flexibi_dst/urls.py
+++ flexibi_dst/urls.py
@@ -2,7 +2,6 @@ import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
 from django.views.generic import RedirectView
 
@@ -14,7 +13,6 @@ admin.site.enable_nav_sidebar = False
 urlpatterns = [
     path('', RedirectView.as_view(url='/home'), name='entry'),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")), ),
     path('home/', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
