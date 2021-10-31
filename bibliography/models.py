3557ea14303f147016f61b6f96f642313fd17234
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-31 21:48:08 +0100

    Added additional fields to Source model
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- bibliography/models.py
+++ bibliography/models.py
@@ -12,6 +12,8 @@ class Licence(models.Model):
 
 
 SOURCE_TYPES = (
+    ('article', 'article'),
+    ('dataset', 'dataset'),
     ('website', 'website'),
     ('custom', 'custom'),
 )
@@ -21,12 +23,17 @@ class Source(models.Model):
     owner = models.ForeignKey(User, default=8, on_delete=models.CASCADE)
     type = models.CharField(max_length=255, choices=SOURCE_TYPES, default='custom')
     authors = models.CharField(max_length=500, null=True)
+    publisher = models.CharField(max_length=127, blank=True, null=True)
     title = models.CharField(max_length=500, null=True)
+    journal = models.CharField(max_length=500, null=True)
+    issue = models.CharField(max_length=255, blank=True, null=True)
+    year = models.IntegerField(null=True)
     abbreviation = models.CharField(max_length=50, null=True)
     abstract = models.TextField(blank=True, null=True)
     licence = models.ForeignKey(Licence, on_delete=models.PROTECT, blank=True, null=True)
     attributions = models.TextField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
+    doi = models.CharField(max_length=255, blank=True, null=True)
     last_accessed = models.DateField(blank=True, null=True)
 
     def get_absolute_url(self):
