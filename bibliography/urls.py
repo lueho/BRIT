8c043f65229f37e07ad53b0c6ed2544dcaccb946
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-10-31 21:43:55 +0100

    Implemented bibliography url naming convention
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- bibliography/urls.py
+++ bibliography/urls.py
@@ -9,9 +9,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('sources/', LiteratureSourceListView.as_view(), name='literature source_list'),
-    path('sources/create/', LiteratureSourceCreateView.as_view(), name='literature source_create'),
-    path('sources/<int:pk>/', LiteratureSourceDetailView.as_view(), name='literature source_detail'),
-    path('sources/<int:pk>/update/', LiteratureSourceUpdateView.as_view(), name='literature source_update'),
-    path('sources/<int:pk>/delete/', LiteratureSourceDeleteView.as_view(), name='literature source_delete'),
+    path('sources/', LiteratureSourceListView.as_view(), name='bib_source_list'),
+    path('sources/create/', LiteratureSourceCreateView.as_view(), name='bib_source_create'),
+    path('sources/<int:pk>/', LiteratureSourceDetailView.as_view(), name='bib_source_detail'),
+    path('sources/<int:pk>/update/', LiteratureSourceUpdateView.as_view(), name='bib_source_update'),
+    path('sources/<int:pk>/delete/', LiteratureSourceDeleteView.as_view(), name='bib_source_delete'),
 ]
