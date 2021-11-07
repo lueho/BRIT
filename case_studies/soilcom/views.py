c44d72320adee255b7d30526aed232e6af50e671
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-11-07 20:24:31 +0100

    Collector CRUD views
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- case_studies/soilcom/views.py
+++ case_studies/soilcom/views.py
@@ -1,31 +1,63 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django_tables2 import SingleTableView
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from bootstrap_modal_forms.generic import BSModalCreateView, BSModalUpdateView, BSModalDeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from brit.views import UserOwnsObjectMixin, NextOrSuccessUrlMixin
 
-from . import models
+from brit import views
 from . import forms
-from . import tables
-from materials.models import Material
+from . import models
 
 
-class CollectorListView(SingleTableView):
-    template_name = 'collectors_list.html'
+# ----------- Collector CRUD -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
+class CollectorListView(views.OwnedObjectListView):
+    template_name = 'simple_list_card.html'
     model = models.Collector
-    table_class = tables.CollectorsTable
-
-    def get_queryset(self):
-        return models.Collector.objects.filter(id=3)
+    permission_required = 'case_studies.soilcom.view_collector'
+    create_new_object_url = reverse_lazy('collector_create')
 
 
-class CollectorCreateView(LoginRequiredMixin, NextOrSuccessUrlMixin, BSModalCreateView):
-    template_name = 'modal_form.html'
+class CollectorCreateView(views.OwnedObjectCreateView):
+    template_name = 'simple_form_card.html'
     form_class = forms.CollectorModelForm
     success_url = reverse_lazy('collector_list')
+    permission_required = 'case_studies.soilcom.add_collector'
 
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
+
+class CollectorModalCreateView(views.OwnedObjectModalCreateView):
+    template_name = 'modal_form.html'
+    form_class = forms.CollectorModalModelForm
+    success_url = reverse_lazy('collector_list')
+    permission_required = 'case_studies.soilcom.add_collector'
+
+
+class CollectorDetailView(views.OwnedObjectDetailView):
+    template_name = 'collector_detail.html'
+    model = models.Collector
+    permission_required = 'case_studies.soilcom.view_collector'
+
+
+class CollectorModalDetailView(views.OwnedObjectModalDetailView):
+    template_name = 'modal_detail.html'
+    model = models.Collector
+    permission_required = 'case_studies.soilcom.view_collector'
+
+
+class CollectorUpdateView(views.OwnedObjectUpdateView):
+    template_name = 'simple_form_card.html'
+    model = models.Collector
+    form_class = forms.CollectorModelForm
+    permission_required = 'case_studies.soilcom.change_collector'
+
+
+class CollectorModalUpdateView(views.OwnedObjectModalUpdateView):
+    template_name = 'modal_form.html'
+    model = models.Collector
+    form_class = forms.CollectorModalModelForm
+    permission_required = 'case_studies.soilcom.change_collector'
+
+
+class CollectorModalDeleteView(views.OwnedObjectDeleteView):
+    template_name = 'modal_delete.html'
+    model = models.Collector
+    success_message = 'Successfully deleted.'
+    success_url = reverse_lazy('collector_list')
+    permission_required = 'case_studies.soilcom.delete_collector'
