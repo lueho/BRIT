73935f48af19c09088908dd46547ec9096b78872
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-11-07 20:23:43 +0100

    Introduced methods for retrieving urls
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- case_studies/soilcom/models.py
+++ case_studies/soilcom/models.py
@@ -1,15 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
 from brit.models import NamedUserObjectModel
 from materials.models import Material
 
 
 class Collector(NamedUserObjectModel):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('collector_detail', args=[self.id])
+
+    @staticmethod
+    def get_create_url():
+        return reverse('collector_create')
+
+    class Meta:
+        verbose_name = 'Waste Collector'
 
 
 class CollectionSystem(NamedUserObjectModel):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('collection_system_detail', args=[self.id])
+
+    class Meta:
+        verbose_name = 'Waste Collection System'
 
 
 class WasteStreamCategory(NamedUserObjectModel):
