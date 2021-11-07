556ba403f75ba59a95576f6745db3b66c028d5a6
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-11-07 20:24:16 +0100

    Collector CRUD paths
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- case_studies/soilcom/urls.py
+++ case_studies/soilcom/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('collectors/', views.CollectorListView.as_view(), name='collector_list'),
     path('collectors/create/', views.CollectorCreateView.as_view(), name='collector_create'),
+    path('collectors/create/modal/', views.CollectorModalCreateView.as_view(), name='collector_create_modal'),
+    path('collectors/<int:pk>/', views.CollectorDetailView.as_view(), name='collector_detail'),
+    path('collectors/<int:pk>/modal/', views.CollectorModalDetailView.as_view(), name='collector_detail_modal'),
+    path('collectors/<int:pk>/update/', views.CollectorUpdateView.as_view(), name='collector_update'),
+    path('collectors/<int:pk>/update/modal/', views.CollectorModalUpdateView.as_view(), name='collector_update_modal'),
+    path('collectors/<int:pk>/delete/modal/', views.CollectorModalDeleteView.as_view(), name='collector_delete_modal'),
 ]
