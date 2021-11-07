943ad6054be54585cde28033017329ffdd5d10e4
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-11-07 20:22:41 +0100

    New custom model form as base class for other model forms
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- case_studies/soilcom/forms.py
+++ case_studies/soilcom/forms.py
@@ -1,7 +1,17 @@
-from bootstrap_modal_forms.forms import BSModalModelForm, BSModalForm
+from bootstrap_modal_forms.forms import BSModalModelForm
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.forms import ModelForm
 
 from . import models
+
+
+class CustomModelForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Save'))
 
 
 class ModalFormHelper(FormHelper):
@@ -11,11 +21,32 @@ class ModalFormHelper(FormHelper):
         self.form_id = 'modal-form'
 
 
-class CollectorModelForm(BSModalModelForm):
-    class Meta:
-        model = models.Collector
-        fields = ('name', 'description')
+class CustomModalModelForm(BSModalModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = ModalFormHelper()
+
+
+class CollectorModelForm(CustomModelForm):
+    class Meta:
+        model = models.Collector
+        fields = ('name', 'description')
+
+
+class CollectorModalModelForm(CustomModalModelForm):
+    class Meta:
+        model = models.Collector
+        fields = ('name', 'description')
+
+
+class CollectionSystemModelForm(CustomModelForm):
+    class Meta:
+        model = models.Collector
+        fields = ('name', 'description')
+
+
+class CollectionSystemModalModelForm(CustomModalModelForm):
+    class Meta:
+        model = models.Collector
+        fields = ('name', 'description')
