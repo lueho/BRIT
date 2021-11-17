7393863d68ce02ba9f5b8d7e3f0afbc3987300c3
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2021-11-17 16:16:31 +0100

    Implemented new models Collection, WasteComponent, WasteStream and WasteFlyer

--- case_studies/soilcom/models.py
+++ case_studies/soilcom/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from brit.models import NamedUserObjectModel
-from materials.models import Material, MaterialSettings
+from bibliography.models import Source
+from maps.models import Catchment
+from materials.models import Material, MaterialGroup, MaterialSettings
 
 
 class Collector(NamedUserObjectModel):
@@ -33,6 +37,31 @@ class WasteCategory(NamedUserObjectModel):
         verbose_name_plural = 'Waste categories'
 
 
+class WasteComponentManager(models.Manager):
+
+    def get_queryset(self):
+        groups = MaterialGroup.objects.filter(name__in=('Biowaste component',))
+        return super().get_queryset().filter(groups__in=groups)
+
+
+class WasteComponent(Material):
+    objects = WasteComponentManager()
+
+    class Meta:
+        proxy = True
+
+    def get_absolute_url(self):
+        return reverse('waste_component_detail', args=[self.id])
+
+
+@receiver(post_save, sender=WasteComponent)
+def add_material_group(sender, instance, created, **kwargs):
+    if created:
+        group = MaterialGroup.objects.get(name='Biowaste component')
+        instance.groups.add(group)
+        instance.save()
+
+
 class WasteStream(NamedUserObjectModel):
     category = models.ForeignKey(WasteCategory, on_delete=models.PROTECT)
     allowed_materials = models.ManyToManyField(Material)
@@ -43,3 +72,35 @@ class WasteStream(NamedUserObjectModel):
 
     class Meta:
         verbose_name = 'Waste Stream'
+
+
+class WasteFlyerManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(type='waste_flyer')
+
+
+class WasteFlyer(Source):
+    objects = WasteFlyerManager()
+
+    class Meta:
+        proxy = True
+        verbose_name = 'Waste Flyer'
+
+    def get_absolute_url(self):
+        return reverse('waste_flyer_detail', args=[self.id])
+
+
+class Collection(NamedUserObjectModel):
+    collector = models.ForeignKey(Collector, on_delete=models.CASCADE, blank=True, null=True)
+    catchment = models.ForeignKey(Catchment, on_delete=models.PROTECT, blank=True, null=True)
+    collection_system = models.ForeignKey(CollectionSystem, on_delete=models.CASCADE, blank=True, null=True)
+    waste_stream = models.ForeignKey(WasteStream, on_delete=models.CASCADE, blank=True, null=True)
+    flyer = models.ForeignKey(WasteFlyer, on_delete=models.CASCADE, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('waste_collection_detail', args=[self.id])
+
+    @property
+    def geom(self):
+        return self.catchment.geom
