c45848b7e9a64d922a48c6c9b9e18a7b2aded366
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2022-04-19 14:45:59 +0200

    Removed non-app-specific static files directory

--- brit/settings/settings.py
+++ brit/settings/settings.py
@@ -121,9 +121,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
