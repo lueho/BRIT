5181d40440cb2ad2ee881522def905ce35bcad3f
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2022-04-20 12:52:44 +0200

    Rearranged to include all migrations to initial data

--- case_studies/soilcom/migrations/0002_add_collection_frequency.py
+++ case_studies/soilcom/migrations/0002_add_collection_frequency.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('soilcom', '0002_initial_data'),
+        ('soilcom', '0001_initial'),
     ]
 
     operations = [
--- case_studies/soilcom/migrations/0003_initial_data.py
+++ case_studies/soilcom/migrations/0003_initial_data.py
@@ -21,7 +21,7 @@ def create_initial_data(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('soilcom', '0001_initial'),
+        ('soilcom', '0002_add_collection_frequency'),
         ('maps', '0001_initial'),
         ('users', '0001_initial_data')
     ]
