d8c7cb54c9d16200bab38eacde701ff25efbe6ea
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2022-07-27 14:47:57 +0200

    file storage with s3

--- brit/settings/settings.py
+++ brit/settings/settings.py
@@ -129,7 +129,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
 
 # Settings for django-registration-redux
 ACCOUNT_ACTIVATION_DAYS = 2
