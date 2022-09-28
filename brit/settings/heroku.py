2e3d7bf40aa838a3e027d1346bf1382b78a6e96c
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2022-09-28 15:57:04 +0200

    Added secure ssl redirects in production

--- brit/settings/heroku.py
+++ brit/settings/heroku.py
@@ -9,6 +9,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ALLOWED_HOSTS.append('bri-tool.herokuapp.com')
 
+SECURE_SSL_REDIRECTS = True
+
 DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
 DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
