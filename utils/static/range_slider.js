8d45e384c44bc6d0f81f64b8ca86d168b887f645
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2022-12-22 17:22:51 +0100

    Added distinction of text between PercentageRangeSlider and regular RangeSlider

--- utils/static/range_slider.js
+++ utils/static/range_slider.js
@@ -7,10 +7,15 @@ $(function() {
         slide: function(event, ui) {
             $("#" + $(this).parent().attr("id") + "_min").val(ui.values[0]);
             $("#" + $(this).parent().attr("id") + "_max").val(ui.values[1]);
-            $("#" + $(this).parent().attr("id") + "_text").text(ui.values[0] + '% - ' + ui.values[1] + '%');
+            if ($("#" + $(this).parent().attr("id") + "_text")[0].classList.contains('percentage-slider-values')) {
+                $("#" + $(this).parent().attr("id") + "_text").text(ui.values[0] + '% - ' + ui.values[1] + '%');
+            } else {
+                $("#" + $(this).parent().attr("id") + "_text").text(ui.values[0] + ' - ' + ui.values[1]);
+            }
+
         },
         create: function(event, ui) {
-            $(this).slider( "option", "step", $(this).parent().data("step") );
+            $(this).slider("option", "step", $(this).parent().data("step"));
             $(this).slider("option", 'min', $(this).parent().data("range_min"));
             $(this).slider("option", 'max', $(this).parent().data("range_max"));
             $(this).slider("option", 'values', [$(this).parent().data("cur_min"), $(this).parent().data("cur_max")]);
