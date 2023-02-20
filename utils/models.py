442e992a8a0e8ee129ea1912df5554200171c12a
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2023-02-20 15:11:10 +0100

    Implemented Unit model

--- utils/models.py
+++ utils/models.py
@@ -1,4 +1,5 @@
 from ai_django_core.models import CommonInfo
+from django.conf import settings
 from django.contrib.auth.models import User, Group
 from django.db import models
 from django.urls import reverse, exceptions
@@ -122,15 +123,34 @@ class NamedUserObjectModel(OwnedObjectModel):
         return self.name
 
 
+class Unit(NamedUserObjectModel):
+    dimensionless = models.BooleanField(default=False, null=True)
+    reference_quantity = models.ForeignKey(
+        'Property',
+        related_name='reference_quantity_in_units',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        unique_together = ['owner', 'name']
+
+
+def get_default_unit_pk():
+    return Unit.objects.get_or_create(
+        owner=get_default_owner(),
+        name=getattr(settings, 'DEFAULT_UNIT_NAME', 'No unit')
+    )[0].pk
+
+
 class Property(NamedUserObjectModel):
     """
     Defines properties that can be shared among other models. Allows to compare instances of different models that share
     the same properties while enforcing the use of matching units.
     """
     unit = models.CharField(max_length=63)
-
-    def __str__(self):
-        return f'{self.name} [{self.unit}]'
+    allowed_units = models.ManyToManyField(Unit)
 
 
 class PropertyValue(NamedUserObjectModel):
@@ -139,6 +159,7 @@ class PropertyValue(NamedUserObjectModel):
     of any other model with a concrete value. Intended to be related to other models through many-to-many relations.
     """
     property = models.ForeignKey(Property, on_delete=models.PROTECT)
+    unit = models.ForeignKey(Unit, on_delete=models.PROTECT, default=get_default_unit_pk)
     average = models.FloatField()
     standard_deviation = models.FloatField(blank=True, null=True)
 
