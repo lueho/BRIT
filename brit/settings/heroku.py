982da8033f29bd91a3e611681f4b43f2c9d20f04
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2023-05-29 20:56:47 +0200

    Changes env variable for database url
    Signed-off-by: Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>

--- brit/settings/heroku.py
+++ brit/settings/heroku.py
@@ -11,11 +11,11 @@ ALLOWED_HOSTS = ALLOWED_HOSTS.append('bri-tool.herokuapp.com')
 
 SECURE_SSL_REDIRECTS = True
 
-DATABASE_COLOR_URL = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.config(default=DATABASE_COLOR_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=500,
+                                              ssl_require=True)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 CRISPY_FAIL_SILENTLY = True
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), databases=False)
