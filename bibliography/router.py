198c4e601e2c2e44864b70a1afa359b4c23e86fa
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2024-02-26 16:24:24 +0100

    Added SourceViewSet

--- bibliography/router.py
+++ bibliography/router.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import (AuthorViewSet, LicenceViewSet)
+from .viewsets import (AuthorViewSet, LicenceViewSet, SourceViewSet)
 
 router = DefaultRouter()
 router.register('authors', AuthorViewSet, basename='api-author')
 router.register('licences', LicenceViewSet, basename='api-licence')
+router.register('sources', SourceViewSet, basename='api-source')
--- bibliography/viewsets.py
+++ bibliography/viewsets.py
@@ -1,7 +1,8 @@
 from utils.viewsets import AutoPermModelViewSet
 
-from .models import Author, Licence
-from .serializers import AuthorModelSerializer, LicenceModelSerializer
+from .filters import SourceModelFilterSet
+from .models import Author, Licence, Source
+from .serializers import AuthorModelSerializer, LicenceModelSerializer, SourceModelSerializer
 
 
 class AuthorViewSet(AutoPermModelViewSet):
@@ -16,6 +17,16 @@ class AuthorViewSet(AutoPermModelViewSet):
 class LicenceViewSet(AutoPermModelViewSet):
     queryset = Licence.objects.all()
     serializer_class = LicenceModelSerializer
+    custom_permission_required = {
+        'list': None,
+        'retrieve': None,
+    }
+
+
+class SourceViewSet(AutoPermModelViewSet):
+    queryset = Source.objects.all()
+    serializer_class = SourceModelSerializer
+    filterset_class = SourceModelFilterSet
     custom_permission_required = {
         'list': None,
         'retrieve': None,
