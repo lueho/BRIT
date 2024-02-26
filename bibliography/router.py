382dbbf3165cfc470b4e80a1f352ca8a292e51c9
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2024-02-26 16:06:14 +0100

    Added LicenceViewSet

--- bibliography/router.py
+++ bibliography/router.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import AuthorViewSet
+from .viewsets import (AuthorViewSet, LicenceViewSet)
 
 router = DefaultRouter()
 router.register('authors', AuthorViewSet, basename='api-author')
+router.register('licences', LicenceViewSet, basename='api-licence')
--- bibliography/viewsets.py
+++ bibliography/viewsets.py
@@ -1,12 +1,21 @@
 from utils.viewsets import AutoPermModelViewSet
 
-from .models import Author
-from .serializers import AuthorModelSerializer
+from .models import Author, Licence
+from .serializers import AuthorModelSerializer, LicenceModelSerializer
 
 
 class AuthorViewSet(AutoPermModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+    custom_permission_required = {
+        'list': None,
+        'retrieve': None,
+    }
+
+
+class LicenceViewSet(AutoPermModelViewSet):
+    queryset = Licence.objects.all()
+    serializer_class = LicenceModelSerializer
     custom_permission_required = {
         'list': None,
         'retrieve': None,
