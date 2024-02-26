352758931716984433067f6c51d744077d2ee66c
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2024-02-26 16:23:58 +0100

    Added SourceModelFilterSet

--- bibliography/filters.py
+++ bibliography/filters.py
@@ -1,21 +1,31 @@
 from django.db.models import Q
-from django_filters import CharFilter
+from django_filters import CharFilter, FilterSet
 
 from utils.filters import BaseCrispyFilterSet
 from .models import Source
 
 
-class SourceFilter(BaseCrispyFilterSet):
+def author_icontains(queryset, _, value):
+    return queryset.filter(
+        Q(authors__last_names__icontains=value) | Q(authors__first_names__icontains=value)
+    ).distinct()
+
+
+class SourceModelFilterSet(FilterSet):
     abbreviation = CharFilter(lookup_expr='icontains')
-    authors = CharFilter(method='author_icontains', label='Author names contain')
+    authors = CharFilter(method=author_icontains, label='Author names contain')
     title = CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Source
         fields = ('abbreviation', 'authors', 'title', 'type', 'year')
 
-    @staticmethod
-    def author_icontains(queryset, _, value):
-        return queryset.filter(
-            Q(authors__last_names__icontains=value) | Q(authors__first_names__icontains=value)
-        ).distinct()
+
+class SourceFilter(BaseCrispyFilterSet):
+    abbreviation = CharFilter(lookup_expr='icontains')
+    authors = CharFilter(method=author_icontains, label='Author names contain')
+    title = CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Source
+        fields = ('abbreviation', 'authors', 'title', 'type', 'year')
