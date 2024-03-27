2db0b944306585678a39317e9e0619964c1db2ed
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2024-03-27 17:59:22 +0100

    Remove unused tables

--- case_studies/flexibi_nantes/tables.py
+++ case_studies/flexibi_nantes/tables.py
@@ -1,56 +1,6 @@
 from django.urls import reverse
 from django.utils.html import format_html
-from django_tables2 import Column, Table, TemplateColumn
-
-
-class StandardGreenhouseTable(Table):
-    """
-    Table template for standard items owned and editable by the current user. This is meant to be a template for the
-    django-tables2 table_factory. Constructor does not work by itself.
-    """
-
-    def __init__(self, *args, **kwargs):
-        name = Column(linkify=lambda record: record.detail_url)
-        growth_cycle_list = Column()
-        kwargs['extra_columns'] = [('name', name), ('growth_cycle_list', growth_cycle_list), ]
-        super().__init__(*args, **kwargs)
-
-    class Meta:
-        fields = ('heated', 'lighted', 'above_ground', 'high_wire')
-        sequence = ('name', '...', )
-
-
-class UserGreenhouseTable(Table):
-    """
-    Table template for items owned and editable by the current user. This is meant to be a template for the
-    django-tables2 table_factory. Constructor does not work by itself.
-    """
-
-    def __init__(self, *args, **kwargs):
-        name = Column(footer=self.get_create_url(), linkify=lambda record: record.detail_url)
-        growth_cycle_list = Column()
-        edit = TemplateColumn(
-            '<a href="javascript:void(0);" class="modal-link" data-link="{{ record.update_url }}?next={{ request.path }}"><i class="fas fa-fw fa-edit"></i></a>')
-        delete = TemplateColumn(
-            '<a href="javascript:void(0);" class="modal-link" data-link="{{ record.delete_url }}"><i class="fas fa-fw fa-trash"></i></a>')
-        kwargs['extra_columns'] = [('name', name), ('growth_cycle_list', growth_cycle_list), ('edit', edit), ('delete', delete)]
-        super().__init__(*args, **kwargs)
-
-    def get_create_url(self):
-        return format_html(
-            '''
-            <a href="javascript:void(0);" class="modal-link" data-link="{0}">
-                <i class="fas fa-fw fa-plus"></i> Create new {1}
-            </a>
-            ''',
-            reverse(f'{self.Meta.model._meta.verbose_name}_create'),
-            # TODO: Find better way, that is not dependent on url pattern convention.
-            self.Meta.model._meta.verbose_name
-        )
-
-    class Meta:
-        fields = ('heated', 'lighted', 'above_ground', 'high_wire')
-        sequence = ('name', '...',)
+from django_tables2 import Column, Table
 
 
 def growthcycle_table_factory(growth_cycle):
