ea49e3c5e901497c61c91b6957e9d9146dd3a979
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2025-03-05 17:53:48 +0100

    Admin for Sources and Authors with Inlines

--- bibliography/admin.py
+++ bibliography/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Author, Source, Licence
+from .models import Author, Licence, Source, SourceAuthor
+
+
+class SourceAuthorInline(admin.TabularInline):
+    model = SourceAuthor
+    extra = 1
+    ordering = ('position',)
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    inlines = [SourceAuthorInline]
+    list_display = ('abbreviation', 'title', 'year')
 
 
 admin.site.register(Author)
 admin.site.register(Licence)
-admin.site.register(Source)
