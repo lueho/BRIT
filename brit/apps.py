ad7f2ecf8ed8aff6dd4aceb8f42b271f65d3b906
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2025-06-03 09:15:55 +0200

    Removed initial data. Initial management command. Initial data is now handled with post_migration signal.

--- brit/apps.py
+++ brit/apps.py
@@ -8,6 +8,3 @@ class BRITConfig(AppConfig):
     def ready(self):
         # Ensure export registry is initialized at startup
         import utils.file_export.registry_init
-
-        # Ensure initial data for tests is loaded
-        call_command("ensure_initial_data")
