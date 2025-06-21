2cde250732e999ef04ea72818958395e26c8f62b
Phillipp Lüssenhop <phillipp.luessenhop@tuhh.de>
2025-06-21 21:23:20 +0200

    feat: add pulping process module with mock views, templates and static assets

--- processes/views.py
+++ processes/views.py
@@ -1,11 +1,30 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import PermissionRequiredMixin
 
 # Create your views here.
 
 # Shared mock process data for dashboard and detail views
 MOCK_PROCESS_TYPES = [
+    {
+        "id": 12,
+        "name": "Pulping",
+        "url_name": "processes:mock_process_overview",
+        "category": "Physicochemical",
+        "input": [
+            {"name": "Straw", "id": 102},
+            {"name": "Wood Chips", "id": 108},
+        ],
+        "output": [],
+        "short_description": "An overview of pulping technologies for disintegrating biomass into fibres.",
+        "description": "This is a summary page for various pulping technologies.",
+        "mechanism": "Physico-chemical or chemical reactions",
+        "temperature_min": 130,
+        "temperature_max": 190,
+        "yield_percentage": "40 - 90%",
+        "sources": [],
+        "info_charts": [],
+    },
     {
         "id": 1,
         "name": "Anaerobic Digestion",
@@ -29,9 +48,13 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Anaerobic Digestion",
-                "url": "https://www.tech4biowaste.eu/wiki/Anaerobic_digestion"
+                "url": "https://www.tech4biowaste.eu/wiki/Anaerobic_digestion",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 2,
@@ -56,9 +79,13 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Gasification",
-                "url": "https://www.tech4biowaste.eu/wiki/Gasification"
+                "url": "https://www.tech4biowaste.eu/wiki/Gasification",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 3,
@@ -84,9 +111,13 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Pyrolysis",
-                "url": "https://www.tech4biowaste.eu/wiki/Pyrolysis"
+                "url": "https://www.tech4biowaste.eu/wiki/Pyrolysis",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 4,
@@ -94,11 +125,9 @@ MOCK_PROCESS_TYPES = [
         "category": "Biochemical",
         "input": [
             {"name": "Organic Waste", "id": 105},
-            {"name": "Green Waste", "id": 110}
+            {"name": "Green Waste", "id": 110},
         ],
-        "output": [
-            {"name": "Compost", "id": 301}
-        ],
+        "output": [{"name": "Compost", "id": 301}],
         "short_description": "Aerobic biological decomposition of organic material to produce compost.",
         "description": "Composting is an aerobic process that converts organic waste into stable, humus-like material called compost, which can be used as a soil conditioner. <a href='https://www.tech4biowaste.eu/wiki/Composting' target='_blank'>Learn more</a>.",
         "mechanism": "Aerobic Decomposition",
@@ -110,20 +139,22 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Composting",
-                "url": "https://www.tech4biowaste.eu/wiki/Composting"
+                "url": "https://www.tech4biowaste.eu/wiki/Composting",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 5,
         "name": "Hydrothermal Processing",
         "category": "Thermochemical",
-        "input": [
-            {"name": "Wet Biomass", "id": 120}
-        ],
+        "input": [{"name": "Wet Biomass", "id": 120}],
         "output": [
             {"name": "Hydrochar", "id": 302},
-            {"name": "Process Water", "id": 303}
+            {"name": "Process Water", "id": 303},
         ],
         "short_description": "Thermochemical conversion of wet biomass under high pressure and moderate temperature.",
         "description": "Hydrothermal processing converts wet biomass into hydrochar and process water under subcritical water conditions. <a href='https://www.tech4biowaste.eu/wiki/Hydrothermal_processing' target='_blank'>Learn more</a>.",
@@ -136,20 +167,20 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Hydrothermal Processing",
-                "url": "https://www.tech4biowaste.eu/wiki/Hydrothermal_processing"
+                "url": "https://www.tech4biowaste.eu/wiki/Hydrothermal_processing",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 6,
         "name": "Torrefaction",
         "category": "Thermochemical",
-        "input": [
-            {"name": "Biomass", "id": 109}
-        ],
-        "output": [
-            {"name": "Torrified Biomass", "id": 304}
-        ],
+        "input": [{"name": "Biomass", "id": 109}],
+        "output": [{"name": "Torrified Biomass", "id": 304}],
         "short_description": "Mild pyrolysis process to improve biomass fuel properties.",
         "description": "Torrefaction is a thermochemical treatment of biomass at 200–320°C in the absence of oxygen, producing a solid fuel with improved properties. <a href='https://www.tech4biowaste.eu/wiki/Torrefaction' target='_blank'>Learn more</a>.",
         "mechanism": "Mild Pyrolysis",
@@ -161,20 +192,20 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Torrefaction",
-                "url": "https://www.tech4biowaste.eu/wiki/Torrefaction"
+                "url": "https://www.tech4biowaste.eu/wiki/Torrefaction",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 7,
         "name": "Steam Explosion",
         "category": "Physical",
-        "input": [
-            {"name": "Lignocellulosic Biomass", "id": 301}
-        ],
-        "output": [
-            {"name": "Exploded Biomass", "id": 305}
-        ],
+        "input": [{"name": "Lignocellulosic Biomass", "id": 301}],
+        "output": [{"name": "Exploded Biomass", "id": 305}],
         "short_description": "Mechanical and thermal pre-treatment using high-pressure steam.",
         "description": "Steam explosion is a pre-treatment process in which biomass is treated with high-pressure steam and then rapidly depressurized, causing the material to break apart. <a href='https://www.tech4biowaste.eu/wiki/Steam_explosion' target='_blank'>Learn more</a>.",
         "mechanism": "Mechanical/Thermal Pre-treatment",
@@ -186,20 +217,20 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Steam Explosion",
-                "url": "https://www.tech4biowaste.eu/wiki/Steam_explosion"
+                "url": "https://www.tech4biowaste.eu/wiki/Steam_explosion",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 8,
         "name": "Ultrasonication",
         "category": "Physical",
-        "input": [
-            {"name": "Sludge", "id": 401}
-        ],
-        "output": [
-            {"name": "Disintegrated Sludge", "id": 402}
-        ],
+        "input": [{"name": "Sludge", "id": 401}],
+        "output": [{"name": "Disintegrated Sludge", "id": 402}],
         "short_description": "Use of ultrasound waves to disintegrate sludge and enhance bioprocesses.",
         "description": "Ultrasonication applies ultrasound waves to sludge or biomass to disrupt cell walls and enhance subsequent bioprocesses. <a href='https://www.tech4biowaste.eu/wiki/Ultrasonication' target='_blank'>Learn more</a>.",
         "mechanism": "Ultrasound Disintegration",
@@ -211,9 +242,13 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Ultrasonication",
-                "url": "https://www.tech4biowaste.eu/wiki/Ultrasonication"
+                "url": "https://www.tech4biowaste.eu/wiki/Ultrasonication",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
     {
         "id": 9,
@@ -221,11 +256,9 @@ MOCK_PROCESS_TYPES = [
         "category": "Material",
         "input": [
             {"name": "Biopolymers", "id": 501},
-            {"name": "Natural Fibres", "id": 502}
+            {"name": "Natural Fibres", "id": 502},
         ],
-        "output": [
-            {"name": "Biocomposite", "id": 503}
-        ],
+        "output": [{"name": "Biocomposite", "id": 503}],
         "short_description": "Manufacturing of composite materials from biopolymers and natural fibres.",
         "description": "Biocomposite processing involves combining biopolymers and natural fibres to create composite materials with enhanced properties. <a href='https://www.tech4biowaste.eu/wiki/Biocomposite_processing' target='_blank'>Learn more</a>.",
         "mechanism": "Composite Manufacturing",
@@ -237,9 +270,13 @@ MOCK_PROCESS_TYPES = [
             {
                 "type": "website",
                 "title": "Tech4Biowaste: Biocomposite Processing",
-                "url": "https://www.tech4biowaste.eu/wiki/Biocomposite_processing"
+                "url": "https://www.tech4biowaste.eu/wiki/Biocomposite_processing",
             }
-        ]
+        ],
+        "info_charts": [
+            {"name": "Info Chart I", "url": "#"},
+            {"name": "Info Chart II", "url": "#"},
+        ],
     },
 ]
 
@@ -249,9 +286,22 @@ class ProcessMockDashboard(PermissionRequiredMixin, TemplateView):
     template_name = "processes/mock_dashboard.html"
 
     def get_context_data(self, **kwargs):
-        return {
-            "process_types": MOCK_PROCESS_TYPES
-        }
+        return {"process_types": MOCK_PROCESS_TYPES}
+
+
+class ProcessOverviewMock(PermissionRequiredMixin, TemplateView):
+    permission_required = "processes.access_app_feature"
+    template_name = "processes/mock_process_overview.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Filter for technologies related to "Pulping"
+        # Using IDs 7 (Steam Explosion), 10, and 11
+        pulping_ids = [7, 10, 11]
+        context["technologies"] = [
+            p for p in MOCK_PROCESS_TYPES if p["id"] in pulping_ids
+        ]
+        return context
 
 
 class ProcessTypeListMock(PermissionRequiredMixin, TemplateView):
@@ -314,6 +364,18 @@ class ProcessTypeListMock(PermissionRequiredMixin, TemplateView):
                     "name": "Biocomposite Processing",
                     "category": "Material",
                     "description": "Manufacturing of composite materials from biopolymers and natural fibres.",
+                },
+                {
+                    "id": 10,
+                    "name": "Horizontal tube digester for straw",
+                    "category": "Pulping",
+                    "description": "Specially designed for straw, giving higher quality fibres for brown and white grades.",
+                },
+                {
+                    "id": 11,
+                    "name": "Liquor circulation digesters for wood",
+                    "category": "Pulping",
+                    "description": "Technology for pulping of wood to fibres of highest quality by Kraft or Sulfite pulping.",
                 },
             ]
         }
@@ -424,6 +486,14 @@ class ProcessMockMaterialDetail(PermissionRequiredMixin, TemplateView):
                 "uses": "Fertilizer, soil conditioner",
                 "related_processes": "Anaerobic Digestion",
             },
+            206: {
+                "name": "Straw",
+                "category": "Agricultural Residue",
+                "description": "Stalks and stems left after grain harvest. Used as feedstock for bioenergy and soil amendment.",
+                "composition": "Cellulose, hemicellulose, silica",
+                "uses": "Bio-oil, animal bedding, compost",
+                "related_processes": "Pulping",
+            },
         }
         return {"material": data.get(material_id)}
 
@@ -434,4 +504,8 @@ class ProcessRunMock(PermissionRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         pt_id = self.kwargs.get("pk")
-        return {"process_type": {"id": pt_id, "name": f"Mock ProcessType #{pt_id}"}} 
+        return {"process_type": {"id": pt_id, "name": f"Mock ProcessType #{pt_id}"}}
+
+
+class StrawAndWoodProcessInfoView(TemplateView):
+    template_name = "processes/pulping_straw_infocard.html"
