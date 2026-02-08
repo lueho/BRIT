be329d3adae45c56ac06086520616270b463a918
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:55:40 +0100

    admin: upgrade flexibi_nantes admin with proper ModelAdmin classes
    - Greenhouse: add name/owner/status to list_display, search_fields, list_filter,
      ordering
    - Culture: register previously unregistered model with full ModelAdmin
    - GreenhouseGrowthCycle: add list_display, list_filter, ordering
    - GrowthTimeStepSet: add list_display, ordering
    - GrowthShare: add list_display, search_fields, ordering
    - Replace bare admin.site.register() with @admin.register() decorator

--- case_studies/flexibi_nantes/admin.py
+++ case_studies/flexibi_nantes/admin.py
@@ -1,18 +1,73 @@
 from django.contrib import admin
 
-from .models import Greenhouse, GreenhouseGrowthCycle, GrowthShare, GrowthTimeStepSet
+from .models import (
+    Culture,
+    Greenhouse,
+    GreenhouseGrowthCycle,
+    GrowthShare,
+    GrowthTimeStepSet,
+)
 
 
 @admin.register(Greenhouse)
 class GreenhouseAdmin(admin.ModelAdmin):
     list_display = (
+        "name",
+        "heated",
+        "lighted",
+        "high_wire",
+        "above_ground",
+        "owner",
+        "publication_status",
+    )
+    search_fields = ("name", "description")
+    list_filter = (
+        "publication_status",
         "heated",
         "lighted",
         "high_wire",
         "above_ground",
     )
+    ordering = ("name",)
 
 
-admin.site.register(GreenhouseGrowthCycle)
-admin.site.register(GrowthTimeStepSet)
-admin.site.register(GrowthShare)
+@admin.register(Culture)
+class CultureAdmin(admin.ModelAdmin):
+    list_display = ("name", "residue", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(GreenhouseGrowthCycle)
+class GreenhouseGrowthCycleAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "cycle_number",
+        "culture",
+        "greenhouse",
+        "owner",
+        "publication_status",
+    )
+    list_filter = ("publication_status",)
+    ordering = ("greenhouse", "cycle_number")
+
+
+@admin.register(GrowthTimeStepSet)
+class GrowthTimeStepSetAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "timestep", "growth_cycle", "owner")
+    ordering = ("growth_cycle", "timestep")
+
+
+@admin.register(GrowthShare)
+class GrowthShareAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "component",
+        "timestepset",
+        "average",
+        "standard_deviation",
+        "owner",
+    )
+    search_fields = ("component__name",)
+    ordering = ("timestepset", "component")
