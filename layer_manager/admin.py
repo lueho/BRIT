3f0d0802dab5876a43545b345c35beeeff718b9f
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:52:19 +0100

    admin: upgrade layer_manager admin with proper ModelAdmin classes
    - Layer: add list_display, search_fields, list_filter, ordering
    - LayerField: add list_display, search_fields, list_filter, ordering
    - LayerAggregatedValue: add list_display, search_fields, ordering
    - Replace bare admin.site.register() with @admin.register() decorator

--- layer_manager/admin.py
+++ layer_manager/admin.py
@@ -2,6 +2,32 @@ from django.contrib import admin
 
 from .models import Layer, LayerAggregatedValue, LayerField
 
-admin.site.register(Layer)
-admin.site.register(LayerField)
-admin.site.register(LayerAggregatedValue)
+
+@admin.register(Layer)
+class LayerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "table_name",
+        "geom_type",
+        "scenario",
+        "feedstock",
+        "algorithm",
+    )
+    search_fields = ("name", "table_name")
+    list_filter = ("geom_type",)
+    ordering = ("name",)
+
+
+@admin.register(LayerField)
+class LayerFieldAdmin(admin.ModelAdmin):
+    list_display = ("field_name", "data_type")
+    search_fields = ("field_name",)
+    list_filter = ("data_type",)
+    ordering = ("field_name",)
+
+
+@admin.register(LayerAggregatedValue)
+class LayerAggregatedValueAdmin(admin.ModelAdmin):
+    list_display = ("name", "value", "unit", "layer")
+    search_fields = ("name",)
+    ordering = ("layer", "name")
