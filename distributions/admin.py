197d21d053d05e5c6f3e31a4bcd084caff86feba
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:51:47 +0100

    admin: upgrade distributions admin with proper ModelAdmin classes
    - TemporalDistribution: add list_display, search_fields, list_filter, ordering
    - Timestep: add list_display, search_fields, list_filter, ordering, autocomplete
    - Period: register previously unregistered model with full ModelAdmin
    - Replace bare admin.site.register() with @admin.register() decorator

--- distributions/admin.py
+++ distributions/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 
-from .models import TemporalDistribution, Timestep
+from .models import Period, TemporalDistribution, Timestep
 
-admin.site.register(TemporalDistribution)
-admin.site.register(Timestep)
+
+@admin.register(TemporalDistribution)
+class TemporalDistributionAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(Timestep)
+class TimestepAdmin(admin.ModelAdmin):
+    list_display = ("name", "distribution", "order", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status", "distribution")
+    ordering = ("distribution", "order")
+    autocomplete_fields = ("distribution",)
+
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "distribution",
+        "first_timestep",
+        "last_timestep",
+        "owner",
+        "publication_status",
+    )
+    list_filter = ("publication_status", "distribution")
+    ordering = ("distribution", "first_timestep")
