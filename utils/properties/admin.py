e2e59bc6bba252b9341c8e335f45ee19297c7b4e
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:52:50 +0100

    admin: upgrade utils/properties admin with proper ModelAdmin classes
    - Unit: add list_display, search_fields, list_filter, ordering
    - Property: add list_display, search_fields, list_filter, ordering,
      filter_horizontal for allowed_units
    - Replace bare admin.site.register() with @admin.register() decorator

--- utils/properties/admin.py
+++ utils/properties/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
-from . import models
+from .models import Property, Unit
 
-admin.site.register(models.Unit)
-admin.site.register(models.Property)
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "dimensionless",
+        "reference_quantity",
+        "owner",
+        "publication_status",
+    )
+    search_fields = ("name",)
+    list_filter = ("publication_status", "dimensionless")
+    ordering = ("name",)
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("name", "unit", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+    filter_horizontal = ("allowed_units",)
