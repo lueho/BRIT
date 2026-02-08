0b7089b7013eb51f2c0cdc49d2a61e0670f9208f
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:56:10 +0100

    admin: register flexibi_hamburg models with proper GISModelAdmin classes
    - HamburgRoadsideTrees: register with list_display, search_fields, list_filter,
      ordering using GISModelAdmin for geometry support
    - HamburgGreenAreas: register with list_display, search_fields, list_filter,
      ordering using GISModelAdmin for geometry support
    - Replace placeholder comment with actual admin registrations

--- case_studies/flexibi_hamburg/admin.py
+++ case_studies/flexibi_hamburg/admin.py
@@ -1,1 +1,34 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
+
+from .models import HamburgGreenAreas, HamburgRoadsideTrees
+
+
+@admin.register(HamburgRoadsideTrees)
+class HamburgRoadsideTreesAdmin(GISModelAdmin):
+    list_display = (
+        "baumid",
+        "gattung_deutsch",
+        "art_deutsch",
+        "pflanzjahr",
+        "stammumfang",
+        "bezirk",
+        "stadtteil",
+    )
+    search_fields = ("baumid", "gattung_deutsch", "art_deutsch", "strasse")
+    list_filter = ("bezirk", "gattung_deutsch")
+    ordering = ("baumid",)
+
+
+@admin.register(HamburgGreenAreas)
+class HamburgGreenAreasAdmin(GISModelAdmin):
+    list_display = (
+        "anlagenname",
+        "belegenheit",
+        "gruenart",
+        "flaeche_ha",
+        "bezirksnummer",
+    )
+    search_fields = ("anlagenname", "belegenheit", "identnummer")
+    list_filter = ("gruenart", "bezirksnummer")
+    ordering = ("anlagenname",)
