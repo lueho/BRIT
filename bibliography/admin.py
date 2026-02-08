e472775da7d9ca786b8169a18ba4500b8ae9a694
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:51:19 +0100

    admin: upgrade bibliography admin with proper ModelAdmin classes
    - Author: add list_display, search_fields, list_filter, ordering
    - Licence: add list_display, search_fields, list_filter, ordering
    - Source: add type/owner/status to list_display, list_filter, ordering,
      autocomplete_fields for licence
    - Replace bare admin.site.register() with @admin.register() decorator

--- bibliography/admin.py
+++ bibliography/admin.py
@@ -12,12 +12,34 @@ class SourceAuthorInline(admin.TabularInline):
     ordering = ("position",)
 
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_names", "first_names", "owner", "publication_status")
+    search_fields = ("last_names", "first_names", "middle_names")
+    list_filter = ("publication_status",)
+    ordering = ("last_names", "first_names")
+
+
+@admin.register(Licence)
+class LicenceAdmin(admin.ModelAdmin):
+    list_display = ("name", "reference_url", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
 @admin.register(Source)
 class SourceAdmin(admin.ModelAdmin):
     inlines = [SourceAuthorInline]
-    list_display = ("abbreviation", "title", "year")
-    search_fields = ("abbreviation", "title")
-
-
-admin.site.register(Author)
-admin.site.register(Licence)
+    list_display = (
+        "abbreviation",
+        "title",
+        "year",
+        "type",
+        "owner",
+        "publication_status",
+    )
+    search_fields = ("abbreviation", "title", "doi")
+    list_filter = ("publication_status", "type")
+    ordering = ("abbreviation",)
+    autocomplete_fields = ("licence",)
