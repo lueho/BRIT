68510bde3b7b27b0571ffc47bd2517d3a9b87245
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-02-08 19:55:05 +0100

    admin: upgrade soilcom admin with proper ModelAdmin classes
    - Collector: add list_display, search_fields, list_filter, ordering
    - CollectionSystem: add list_display, search_fields, list_filter, ordering
    - WasteCategory: add list_display, search_fields, list_filter, ordering
    - WasteComponent: add list_display, search_fields, list_filter, ordering
    - WasteStream: add list_display, search_fields, list_filter, ordering,
      filter_horizontal
    - WasteFlyer: add list_display, search_fields, list_filter, ordering
    - CollectionSeason: add list_display, list_filter, ordering
    - CollectionFrequency: add list_display, search_fields, list_filter, ordering
    - FeeSystem: add list_display, search_fields, list_filter, ordering
    - Collection: add list_display, search_fields, list_filter, ordering,
      autocomplete_fields, filter_horizontal
    - CollectionPropertyValue: register previously unregistered model
    - AggregatedCollectionPropertyValue: register previously unregistered model
    - Replace bare admin.site.register() with @admin.register() decorator

--- case_studies/soilcom/admin.py
+++ case_studies/soilcom/admin.py
@@ -1,14 +1,148 @@
 from django.contrib import admin
 
-from . import models
+from .models import (
+    AggregatedCollectionPropertyValue,
+    Collection,
+    CollectionFrequency,
+    CollectionPropertyValue,
+    CollectionSeason,
+    CollectionSystem,
+    Collector,
+    FeeSystem,
+    WasteCategory,
+    WasteComponent,
+    WasteFlyer,
+    WasteStream,
+)
 
-admin.site.register(models.Collector)
-admin.site.register(models.CollectionFrequency)
-admin.site.register(models.CollectionSystem)
-admin.site.register(models.CollectionSeason)
-admin.site.register(models.FeeSystem)
-admin.site.register(models.WasteCategory)
-admin.site.register(models.WasteComponent)
-admin.site.register(models.WasteStream)
-admin.site.register(models.WasteFlyer)
-admin.site.register(models.Collection)
+
+@admin.register(Collector)
+class CollectorAdmin(admin.ModelAdmin):
+    list_display = ("name", "website", "catchment", "owner", "publication_status")
+    search_fields = ("name", "description")
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(CollectionSystem)
+class CollectionSystemAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(WasteCategory)
+class WasteCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(WasteComponent)
+class WasteComponentAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(WasteStream)
+class WasteStreamAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "owner", "publication_status")
+    search_fields = ("name", "category__name")
+    list_filter = ("publication_status", "category")
+    ordering = ("name",)
+    filter_horizontal = ("allowed_materials", "forbidden_materials", "composition")
+
+
+@admin.register(WasteFlyer)
+class WasteFlyerAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "url", "url_valid", "owner", "publication_status")
+    search_fields = ("url", "title")
+    list_filter = ("publication_status", "url_valid")
+    ordering = ("url",)
+
+
+@admin.register(CollectionSeason)
+class CollectionSeasonAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "distribution",
+        "first_timestep",
+        "last_timestep",
+        "owner",
+        "publication_status",
+    )
+    list_filter = ("publication_status",)
+    ordering = ("first_timestep",)
+
+
+@admin.register(CollectionFrequency)
+class CollectionFrequencyAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status", "type")
+    ordering = ("name",)
+
+
+@admin.register(FeeSystem)
+class FeeSystemAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "publication_status")
+    search_fields = ("name",)
+    list_filter = ("publication_status",)
+    ordering = ("name",)
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "collector",
+        "catchment",
+        "collection_system",
+        "valid_from",
+        "valid_until",
+        "owner",
+        "publication_status",
+    )
+    search_fields = ("name", "collector__name", "catchment__name")
+    list_filter = ("publication_status", "connection_type", "collection_system")
+    ordering = ("name",)
+    autocomplete_fields = ("sources",)
+    filter_horizontal = ("samples", "flyers", "predecessors")
+
+
+@admin.register(CollectionPropertyValue)
+class CollectionPropertyValueAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "collection",
+        "property",
+        "average",
+        "year",
+        "owner",
+        "publication_status",
+    )
+    search_fields = ("collection__name", "property__name")
+    list_filter = ("publication_status",)
+    ordering = ("collection", "property")
+    autocomplete_fields = ("sources",)
+
+
+@admin.register(AggregatedCollectionPropertyValue)
+class AggregatedCollectionPropertyValueAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "property",
+        "average",
+        "year",
+        "owner",
+        "publication_status",
+    )
+    search_fields = ("property__name",)
+    list_filter = ("publication_status",)
+    ordering = ("property",)
+    autocomplete_fields = ("sources",)
+    filter_horizontal = ("collections",)
