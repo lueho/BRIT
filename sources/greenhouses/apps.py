ddcb402e1bf976d06e17b6d25ea68a04d3c7e284
Phillipp Lüssenhop <phillipp@luessenhop.eu>
2026-03-08 13:57:45 +0100

    Scaffold nested sources domain apps

---
+++ sources/greenhouses/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class GreenhousesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sources.greenhouses"
+    verbose_name = "Sources / Greenhouses"
---
+++ sources/waste_collection/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WasteCollectionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sources.waste_collection"
+    verbose_name = "Sources / Waste Collection"
